649b4bc79e00ffb8f8cd8cdcca27132dc10ea8a6
Tianji <zhang.sh.tianji@gmail.com>
2021-10-11 16:04:14 -0400

    Create UserManager class

--- src/main/java/UserManager.java
+++ src/main/java/UserManager.java
@@ -1,2 +1,39 @@
+import java.util.HashMap;
+
 public class UserManager {
+    private HashMap<String, User> userMap;
+
+    /**
+     * Construct a UserMap given a HashMap.
+     *
+     * @param userMap HashMap of users.
+     */
+    public UserManager(HashMap<String, User> userMap){
+        this.userMap = userMap;
+    }
+
+    /**
+     * Construct a new UserMap.
+     *
+     *
+     */
+    public UserManager(){
+        this.userMap = new HashMap<>();
+    }
+
+    /**
+     * Return the User based on username.
+     *
+     * @return User
+     **/
+    public User getUser(String username){ return userMap.get(username); }
+
+    /**
+     * Add a new user to the HashMap.
+     *
+     *
+     **/
+    public void addUser(User newUser){ userMap.put(newUser.getUsername(), newUser);}
+
+
 }
