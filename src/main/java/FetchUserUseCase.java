119a56af1b2a65a50caf01dd15a1b6ec59432604
michaelng0107 <33400111+michaelng0107@users.noreply.github.com>
2021-11-04 13:12:02 -0400

    file beloning to the previous commit

--- src/main/java/FetchUserUseCase.java
+++ src/main/java/FetchUserUseCase.java
@@ -6,13 +6,9 @@ public class FetchUserUseCase {
         this.database = database;
     }
 
-    public User getUser(String username) {
-        try {
-            String[] userInfo = database.findUser(username);
-            User user = new User(userInfo[0], userInfo[1], userInfo[2] ,userInfo[3]);
-            return user;
-        } catch (UserDoesNotExistException e) {
-            return null;
-        }
+    public User getUser(String username) throws UserDoesNotExistException {
+        String[] userInfo = database.findUser(username);
+        User user = new User(userInfo[0], userInfo[1], userInfo[2] ,userInfo[3]);
+        return user;
     }
 }
