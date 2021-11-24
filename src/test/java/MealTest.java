5e3616259a16494688a90a88bc231d2e093d56e4
pillscapsules <pirooz321@gmail.com>
2021-11-24 01:51:27 -0500

    (WIP) Test cases for Day and Meal class.

---
+++ src/test/java/MealTest.java
@@ -0,0 +1,37 @@
+import Schedule.Meal;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealTest {
+
+    private Meal meal1;
+    private Meal meal2;
+    private Meal meal3;
+    @BeforeEach
+    void setUp() {
+        meal1 = new Meal("Sugar", 4);
+        meal2 = new Meal("Burger", 9999);
+        meal3 = new Meal("Salt", 0);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+        assert meal1.getName().equals("Sugar");
+        assert meal2.getName().equals("Burger");
+        assert meal3.getName().equals("Salt");
+    }
+
+    @Test
+    void getCalories() {
+        assert meal1.getCalories() == 4;
+        assert meal2.getCalories() == 9999;
+        assert meal3.getCalories() == 0;
+    }
+}
