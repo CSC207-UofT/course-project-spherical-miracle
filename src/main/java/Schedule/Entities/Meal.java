dc2181cfeb7b8bf4eb2461ae65403a449e5912af
Tianji <zhang.sh.tianji@gmail.com>
2021-12-05 21:55:29 -0500

    Add Missing JavaDocs and remove no longer unused variables.

--- src/main/java/Schedule/Entities/Meal.java
+++ src/main/java/Schedule/Entities/Meal.java
@@ -6,9 +6,6 @@ package Schedule.Entities;
 
 public class Meal extends AbstractScheduleEntity implements ScheduleEntity{
 
-    private String name;
-    private int calories;
-
     /**
      * Constructs a Meal with the given name and the calories in it.
      * @param name name of the Meal
